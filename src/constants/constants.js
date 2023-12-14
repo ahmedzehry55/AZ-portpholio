025e9e3712951fdfe876b9b46178ff589e8f3c32
Ahmed Zehry <ahmedalhlwany2111@gmail.com>
2023-12-14 16:09:21 +0200

    travelapp

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,13 +2,22 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 
 export const projects = [
   {
+    title: 'Travel App',
+    description: "ui/ux design for travel app .",
+      image: '/travelapp.png',
+      tags: ['HTML', 'CSS','JavaSctipt' ,'React' ,'NEXT.JS','Tailwind','TypeScript'],
+    source: 'https://github.com/ahmedzehry55/travelapp',
+    visit: 'https://travelapp-tau.vercel.app/',
+    id: 0,
+  },
+  {
     title: 'Notes App',
     description: "this app can helping you to save your notes or and save it on browser local storage .",
       image: '/noteapp.png',
       tags: ['HTML', 'CSS','JavaSctipt' ,'React'],
     source: 'https://github.com/ahmedzehry55/notesApp',
     visit: 'https://ahmedzehry55.github.io/notesApp/',
-    id: 0,
+    id: 1,
   },
   {
     title: 'Baba abdo restaurant menu',
@@ -17,7 +26,7 @@ export const projects = [
     tags: ['HTML', 'CSS','JavaSctipt' ,'React'],
     source: 'https://github.com/ahmedzehry55/babaAbdo',
     visit: 'https://ahmedzehry55.github.io/babaAbdo/',
-    id: 1,
+    id: 2,
   },
   {
     title: 'GPT-3',
@@ -26,7 +35,7 @@ export const projects = [
       tags: ['HTML', 'CSS','JavaSctipt' ,'React'],
     source: 'https://github.com/ahmedzehry55/gpt3',
     visit: 'https://ahmedzehry55.github.io/gpt3/',
-    id: 2,
+    id: 3,
   },
   {
     title: 'Hoo Bank',
@@ -35,7 +44,7 @@ export const projects = [
     tags: ['HTML', 'CSS','JavaSctipt' ,'React'],
     source: 'https://github.com/ahmedzehry55/AZ_hoobank',
     visit: 'https://ahmedzehry55.github.io/AZ_hoobank/',
-    id: 3,
+    id: 4,
   },
 ];
 
